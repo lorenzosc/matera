76a4737d2cc1cf6d42fd4c0ccfe4131b726cec2b
lorenzosc <lorenzocorrea6@hotmail.com>
2024-09-24 08:46:52 -0300

    fix: remove uneditable fields from serializers

--- loan/api/serializers.py
+++ loan/api/serializers.py
@@ -4,9 +4,9 @@ from .models import Loan, Payment
 class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
-        fields = '__all__'
+        exclude = ['user', 'id']
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = '__all__'
+        exclude = ['user']
